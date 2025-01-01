fc94134aa399b448a27cd886e92b1bcb8d020e40
Hector Brito <hector-brito@hector.brito>
2024-12-31 23:23:45 -0300

    use Groups instead of class instances

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+
 from constants import *
 from player import Player
 
@@ -10,8 +11,13 @@ def main():
     pygame.init()
     time = pygame.time.Clock()
     dt = 0
-    
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
@@ -19,11 +25,17 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
-        player.draw(screen)
-        pygame.display.flip()
+
         dt = time.tick(60) / 1000
-        player.update(dt)
+        screen.fill((0, 0, 0))
+
+        for entity in updatable:
+            entity.update(dt)
+
+        for entity in drawable:
+            entity.draw(screen)
+
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
